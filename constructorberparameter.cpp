c4c535599f5257b4d6db8b0ef970438b734922f8
MDenyFerdyHalim <mdenyferdy@gmail.com>
2025-05-13 18:52:37 +0700

    7

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -2,8 +2,11 @@
 #include <string>
 using namespace std;
 
-class mahsiswa {
+class mahasiswa {
 public:
     // Constructor dengan parameter
     mahasiswa(int nim, string nama);
-}
+};
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama) {}
